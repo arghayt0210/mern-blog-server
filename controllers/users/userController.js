f8f0cf1e6156b30d174b2de82c00ded1128d075a
arghayt0210 <argha.yt.0210@gmail.com>
2024-11-10 17:57:08 +0530

    backend logic for registration and login with passportjs

---
+++ controllers/users/userController.js
@@ -0,0 +1,71 @@
+const User = require("../../models/user/user");
+const asyncHandler = require("express-async-handler");
+const CustomError = require("../../utils/CustomError");
+const bcrypt = require("bcryptjs");
+const passport = require("../../utils/password-config");
+const jwt = require("jsonwebtoken");
+
+const register = asyncHandler(async (req, res) => {
+  // check if username already exists
+  const { username, email, password } = req.body;
+  const user = await User.findOne({ username, email });
+  if (user) {
+    throw new CustomError("User already exists", 400);
+  }
+
+  // Hash password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // Create user
+  const newUser = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  res.status(201).json({
+    status: "success",
+    message: "User registered successfully",
+    data: newUser,
+  });
+});
+
+const login = asyncHandler(async (req, res, next) => {
+  const { username, password } = req.body;
+
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    // if user not found
+    if (!user) {
+      throw new CustomError(info.message, 401);
+    }
+
+    // generate token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    // set token in cookie
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "strict",
+      maxAge: 24 * 60 * 60 * 1000, // 1 day
+    });
+
+    res.status(200).json({
+      status: "success",
+      message: "Login successful",
+      data: {
+        username: user.username,
+        email: user.email,
+        _id: user._id,
+      },
+    });
+  })(req, res, next);
+});
+
+module.exports = {
+  register,
+  login,
+};
--- server.js
+++ server.js
@@ -3,9 +3,11 @@ require("dotenv").config();
 const Post = require("./models/post/post");
 const connectDb = require("./utils/connectDb");
 const cors = require("cors");
+const passport = require("./utils/password-config");
 const { default: mongoose } = require("mongoose");
 const CustomError = require("./utils/CustomError");
 const postRouter = require("./router/post/postRouter");
+const userRouter = require("./router/users/userRouter");
 
 connectDb();
 
@@ -23,8 +25,12 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//! Passport middleware
+app.use(passport.initialize());
+
 //! Routes
 app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/users", userRouter);
 
 //! Error Handling Middleware
 app.use((err, req, res, next) => {
